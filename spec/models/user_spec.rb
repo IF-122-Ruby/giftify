15b1bce33678f81ca7f39e3bab5875f31faf4626
Sonia <glushchak02@gmail.com>
2021-05-24 11:47:15 +0300

    add shoulda-matchers for model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(first_name: 'Name', last_name: 'Surname', email: 'test@gmail.com')}
-
-  it 'is invalid with incorrect data' do
-    expect(user.invalid?).to be_truthy
-  end
-
-  it 'is valid with correct data' do
-    user.password = '123456'
-
-    expect(user.valid?).to be_truthy
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
   end
 end

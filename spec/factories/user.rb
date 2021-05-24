738fe886c90d2ca9ea12c24592e57a4b6a748371
Vasylenchuk <38836502+Vasylenchuk@users.noreply.github.com>
2021-05-24 11:43:14 +0300

    Update factory in spec/factories/user.rb
    Co-authored-by: manastyretskyi <43040379+manastyretskyi@users.noreply.github.com>

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :random_user, class: User do
+  factory :user, class: User do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     birthday { Faker::Date.birthday(min_age: 18, max_age: 65)}

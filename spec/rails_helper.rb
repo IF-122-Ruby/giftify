4f0a72292395359c080831c2840976230f737d2f
Cassinipy <mischa.blaster@gmail.com>
2021-05-24 11:02:24 +0300

    install react

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,10 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
+  # Ensure that if we are running js tests, we are using latest webpack assets
+  # This will use the defaults of :js and :server_rendering meta tags
+  ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
@@ -21,4 +25,3 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 end
-

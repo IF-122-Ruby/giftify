078306a0b6fbf7727113a50c604c24b0d9438716
SerhiiPosternak <goextremeforall@gmail.com>
2021-05-25 14:45:42 +0300

    add bootstrap5, generate staticpage, admin layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/pricing'
+  get 'static_pages/admin'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

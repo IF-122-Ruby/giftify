2770c44790d7e4158bcc106ae63a54627359622b
SerhiiPosternak <goextremeforall@gmail.com>
2021-05-30 15:38:31 +0300

    add parshals content, developers, statistic,

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,4 @@
+class AdminController < ActionController::Base
+  def admin
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+  get '/admin', to: 'admin#admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

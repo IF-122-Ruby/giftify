6ad307abe84693ed7f15d715538c59c3fba4869d
Diana <dianaporogy@gmail.com>
2021-05-31 13:55:00 +0300

    Last changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end

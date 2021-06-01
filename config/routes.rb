e4ed881e622ca575f7ac792bb456acd2d8f1e44b
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-01 12:57:09 +0300

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  get '/admin', to: 'admin#admin'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

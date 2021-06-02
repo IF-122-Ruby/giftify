b11d0948475bbe40ef960f7b338cc4f7fb27c879
dimk0o_ <dima.azarenkoo@gmail.com>
2021-06-02 16:38:29 +0300

    installed font awesome with webpacker and made some minor corrections due to the comments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Rails.start()
 Turbolinks.start()

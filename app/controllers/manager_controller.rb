d697d38f4d5400a8c4ee35bf250eb6e6e44035ac
Diana <dianaporogy@gmail.com>
2021-06-03 10:08:29 +0300

    Try make sign up

---
+++ app/controllers/manager_controller.rb
@@ -0,0 +1,17 @@
+class ManagerController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+end

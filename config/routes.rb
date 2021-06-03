1fcb2a58b7958a549d3404e22874608f5ea65a08
everlastroman <everlast.roman@gmail.com>
2021-06-03 19:11:25 +0300

    add column to feedback, change static_pages controller(add feedback a… (#16)
    * add column to feedback, change static_pages controller(add feedback actions), seeds, rspec test for static_pages controller
    
    * fix all comments
    
    * Update app/controllers/feedbacks_controller.rb
    
    * add attributes_for to rspec test feedback
    
    * Update seeds.rb
    
    * Update spec/requests/feedbacks_spec.rb
    
    * fix last review
    
    * add constant reason to feedback model
    
    * constant change
    
    * change factory, spectest, constant for feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'about', to: 'static_pages#about'
-  
+
+  resource :feedback, path: :contact_us, only: [:create, :new]
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }

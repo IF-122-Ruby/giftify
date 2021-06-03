57dc42f058c323eb9021c503e4aace0680206a77
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-03 14:54:15 +0300

    add helper for public_pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,16 +1,6 @@
 class AdminController < ActionController::Base
-
   def admin
     @users = User.all
     @organizations = Organization.all
-
-    @developers = [{name: "Mykhailo",last_name: "Vasylenchuk", avatar: "https://i.ibb.co/7j2wy6F/2.png"},
-                   {name: "Dmutro",last_name: "Azarenko", avatar: "https://i.ibb.co/VTmPhRT/index.jpg"},
-                   {name: "Diana",last_name: "Hodovanets", avatar: "https://i.ibb.co/XV3RGTy/1.jpg"},
-                   {name: "Denys",last_name: "Melnychuk", avatar: "https://i.ibb.co/Q9qd20X/3.png"},
-                   {name: "Roman",last_name: "Panko", avatar: "https://i.ibb.co/wLy8FvZ/index.jpg"},
-                   {name: "Vasul",last_name: "Yatsushun", avatar: "https://i.ibb.co/M1ThK0Y/index.jpg"},
-                   {name: "Sofiia",last_name: "Hlushchak", avatar: "https://i.ibb.co/cvGvXKC/2.jpg"},
-                   {name: "Serhii",last_name: "Posternak", avatar: "https://i.ibb.co/QP1xyGT/1.png"}]
   end
 end

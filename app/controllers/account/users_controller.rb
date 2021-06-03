c654688c43fb585e7ffe241e5470ccf1035811e8
Hlushchak Sofiia <52669896+SofiiaHlushchak@users.noreply.github.com>
2021-06-03 22:46:45 +0300

    implamentation crud for user in account namespace (#15)
    * implamentation crud for user in account namespace
    
    * fix some comments
    
    * fix comments
    
    * fix test
    
    * add context for validations (admin, manager)

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,18 +1,65 @@
 class Account::UsersController < Account::AccountsController
-  before_action :authorize_user
   
   def index
-    @users = current_organization.users.all
+    @users = collection
+    authorize([:account, @users])
   end
 
   def show
-    @user = current_organization.users.find(params[:id])
-    authorize([:account, current_user])
+    @user = resource
+    authorize([:account, @user])
+  end
+
+  def edit
+    @user = resource
+    authorize([:account, @user])
+  end
+
+  def update
+    @user = resource
+    authorize([:account, @user])
+    @user.assign_attributes(user_update_params)
+
+    respond_to do |format|
+      if @user.save(context: user_role_context)
+        format.html { redirect_to account_user_path, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  def destroy
+    @user = resource
+    authorize([:account, @user])
+
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to account_users_path, notice: 'User was successfully destroyed.' }
+    end
   end
 
   private
 
-  def authorize_user 
-    authorize([:account, current_user])
+  def user_role_context
+    current_user.role.admin? ? :admin_context : :manager_context
+  end
+
+  def collection
+    current_organization.users
+  end
+
+  def resource
+    collection.find(params[:id])
+  end
+
+  def user_update_params
+    params.require(:user).permit( :first_name,
+                                  :last_name,
+                                  :email,
+                                  :birthday,
+                                  :password,
+                                  :password_confirmation,
+                                  role_attributes: %i[id role]) 
   end
 end
--- spec/requests/account/users_controller_spec.rb
+++ spec/requests/account/users_controller_spec.rb
@@ -1,16 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Account::UsersController, type: :request do
-  let(:admin) { create(:user, :admin) }
+  let!(:admin) { create(:user, :admin) }
 
   before do
     sign_in admin
   end
 
-  describe "users" do
+  describe 'GET #index' do
     it "returns http success if signed in as admin" do
-      get '/account/users'
-      expect(response).to have_http_status(:success)
+      get account_users_path
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success status' do
+      get account_user_path(admin)
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'renders edit template' do
+      get edit_account_user_path(admin)
+
+      expect(response).to be_successful 
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:user_update_params)     { attributes_for :user }
+    let(:edited_user_first_name) { user_update_params[:first_name] }
+
+    it 'shound update user (in this case admin)' do
+      patch account_user_path(admin), params: { user: user_update_params }
+
+      admin.reload
+      expect(admin.first_name).to eq edited_user_first_name
+    end
+  end
+
+
+  describe 'DELETE #destroy' do
+    let(:new_user) { create(:user, :simple) }
+
+    before { admin.organization.users << new_user }
+
+    it 'should destroy users from admin`s organization' do
+      expect do
+        delete account_user_path(new_user)
+      end.to change(User, :count).by(-1)
     end
   end
 end

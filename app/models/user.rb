82ffbd67d8fc2ecef786207fcfc9b8caefe6f920
everlastroman <everlast.roman@gmail.com>
2021-06-04 17:12:57 +0300

    add and configure any_login gem (#24)
    * add and configure any_login gem
    
    * change type querry in scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,11 @@
 #
 
 class User < ApplicationRecord
+  scope :admins, -> { joins(:role).where(roles: { role: Role::ADMIN }) }
+  scope :managers, -> { joins(:role).where(roles: { role: Role::MANAGER }) }
+  scope :users, -> { joins(:role).where(roles: { role: Role::USER }) }
+  scope :superadmins, -> { joins(:role).where(roles: { role: Role::SUPERADMIN }) }
+
   has_one :role, dependent: :destroy
   has_one :owned_organization, class_name: 'Organization'
   has_one :organization, through: :role
@@ -33,5 +38,14 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :owned_organization 
   accepts_nested_attributes_for :role, reject_if: :all_blank
+
+  def self.grouped_collection_by_role
+    {
+      'admin' => User.admins,
+      'manager' => User.managers,
+      'user' => User.users,
+      'superadmin' => User.superadmins
+    }
+  end
 end
 

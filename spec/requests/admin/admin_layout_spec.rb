f40166cc76c3bf2a2829aa8d6c421f806c432843
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-04 12:11:31 +0300

    fix text spec for admin

--- spec/requests/admin/admin_layout_spec.rb
+++ spec/requests/admin/admin_layout_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Render admin layout", :type => :request do
 
   it "Must render admin layout, get response" do
     get "/admin"
-    expect(response)
+    expect(response).to have_http_status(:success)
     expect(response.body).to include("<h2>Menaging organizations</h2>")
   end
 end

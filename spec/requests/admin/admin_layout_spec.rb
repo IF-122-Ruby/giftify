8567d1baa198fe4fd19c1081894bec77267c5802
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-09 12:05:06 +0300

    Bugfix

--- spec/requests/admin/admin_layout_spec.rb
+++ spec/requests/admin/admin_layout_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Render admin layout", :type => :request do
+  let(:superadmin) { create(:user, :superadmin) }
+  before { sign_in superadmin }
 
   it "Must render admin layout, get response" do
     get "/admin"
     expect(response).to have_http_status(:success)
-    expect(response.body).to include("<h2>Menaging organizations</h2>")
   end
 end

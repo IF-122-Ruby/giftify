7551cbf9e757b090d895cf6a563ec5f4a8f19117
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-10 00:15:11 +0300

    fixed bugs

---
+++ app/javascript/packs/account.js
@@ -0,0 +1,4 @@
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "../stylesheets/account/admin";
+import "bootstrap";
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -13,7 +10,6 @@ import "@fortawesome/fontawesome-free/js/all";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap";
 import "../stylesheets/application";
-import "../stylesheets/account/admin";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()

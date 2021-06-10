697dd5b128c90b4da90f26a1c27b4797f7ccf405
everlastroman <everlast.roman@gmail.com>
2021-06-10 14:50:17 +0300

    last fix any_login

--- config/initializers/any_login.rb
+++ config/initializers/any_login.rb
@@ -3,7 +3,7 @@ AnyLogin.setup do |config|
   # config.provider = nil
 
   # # enabled or not
-  config.enabled = Rails.env
+  config.enabled = Rails.env.to_s == ENV["RAILS_ENV"]
 
   # # Account, User, Person, etc
   # config.klass_name = 'User'

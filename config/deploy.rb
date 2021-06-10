ff304eb6f31b556160d103eb53122f4d2138ad0f
everlastroman <everlast.roman@gmail.com>
2021-06-10 14:28:05 +0300

    last fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:IF-122-Ruby/giftify.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'deploy_on_production'
+set :branch, 'main'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/#{fetch :application}"
 

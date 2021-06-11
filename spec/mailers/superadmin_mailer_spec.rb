fb3f567fb1cd2288b6229832238b1157c5676804
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-11 19:28:18 +0300

    add test

--- spec/mailers/superadmin_mailer_spec.rb
+++ spec/mailers/superadmin_mailer_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe SuperadminMailer, type: :mailer do
   describe "send_mail_when_new_feedback_created" do
     let(:mail) { SuperadminMailer.send_mail_when_new_feedback_created(create(:feedback)) }
-    
-    
+
+
     describe 'superadmins exists' do
       let!(:superadmins) { create_list(:user, 10, :superadmin) }
 
@@ -16,6 +16,24 @@ RSpec.describe SuperadminMailer, type: :mailer do
 
       it "renders the body" do
         expect(mail.body.encoded).to match("sent feedback")
+      end
+    end
+  end
+
+  describe "send_mail_when_new_organization_created" do
+    let(:mail) { SuperadminMailer.send_mail_when_new_organization_created(create(:organization)) }
+
+    describe 'superadmins exists' do
+      let!(:superadmins) { create_list(:user, 10, :superadmin) }
+
+      it "renders the headers" do
+        expect(mail.subject).to eq("Someone create new organization!")
+        expect(mail.to).to eq(superadmins.pluck(:email))
+        expect(mail.from).to eq(["giftify@mail.com"])
+      end
+
+      it "renders the body" do
+        expect(mail.body.encoded).to match("was successfully created")
       end
     end
   end

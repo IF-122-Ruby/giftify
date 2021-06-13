ea38b0be7832a216d46de455b8215685f01232e7
Cassinipy <mischa.blaster@gmail.com>
2021-06-14 00:57:55 +0300

    bootstrap styling to devise forms

--- app/javascript/packs/devise.js
+++
@@ -1,9 +0,0 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import "channels"
-import "@fortawesome/fontawesome-free/js/all";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "../stylesheets/account/devise";
-import "bootstrap";
-Rails.start()
-Turbolinks.start()

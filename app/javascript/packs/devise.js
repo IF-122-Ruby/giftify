0ca291e08f03d1f00085806c2600eeba972218ea
Cassinipy <mischa.blaster@gmail.com>
2021-06-13 14:15:21 +0300

    some style changes

---
+++ app/javascript/packs/devise.js
@@ -0,0 +1,9 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import "channels"
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "../stylesheets/account/devise";
+import "bootstrap";
+Rails.start()
+Turbolinks.start()

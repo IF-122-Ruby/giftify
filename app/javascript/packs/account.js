19bf2fb447eb709ebf061b94381743774540df65
Diana <dianaporogy@gmail.com>
2021-06-14 02:46:48 +0300

    Header and sidebar

--- app/javascript/packs/account.js
+++ app/javascript/packs/account.js
@@ -1,9 +1,9 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
+import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "../stylesheets/account/admin";
-import "bootstrap";
 Rails.start()
 Turbolinks.start()
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: "localhost",

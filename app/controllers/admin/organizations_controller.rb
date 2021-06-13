6a2fc25ca694495c69676baf2e47136685f4b3cf
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-13 15:19:44 +0300

    change rocket to :, fix bug with sidebar height

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrganizationsController < Admin::BaseController
   def index
-    @organizations = Organization.all.paginate(:page => params[:page], :per_page => 5)
+    @organizations = Organization.all.paginate(page: params[:page], per_page: 5)
   end
 
   def show

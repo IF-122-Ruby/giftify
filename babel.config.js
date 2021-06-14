beb8d9cf33f9aa94283633d8b874d0301ec03880
Diana <dianaporogy@gmail.com>
2021-06-14 14:29:14 +0300

    Fix comment

--- babel.config.js
+++ babel.config.js
@@ -24,7 +24,8 @@ module.exports = function(api) {
             node: 'current'
           },
           modules: 'commonjs'
-        }
+        },
+        '@babel/preset-react'
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -34,6 +35,13 @@ module.exports = function(api) {
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
+        }
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: isDevelopmentEnv || isTestEnv,
+          useBuiltIns: true
         }
       ]
     ].filter(Boolean),

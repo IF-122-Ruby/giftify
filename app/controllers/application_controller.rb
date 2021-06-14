68ca04aea51602df16dbcc61d9b52e6f627d6634
Cassinipy <mischa.blaster@gmail.com>
2021-06-14 14:12:32 +0300

    fix comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
-  layout :layout_by_resource
-  
   include Pundit
   
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-
-  def layout_by_resource
-    if devise_controller?
-      'devise'
-    else
-      'application'
-    end
-  end
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."

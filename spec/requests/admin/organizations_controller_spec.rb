86146ddd14b6987f291ff4504f9da71f1be49acd
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-14 13:23:39 +0300

    change test

--- spec/requests/admin/organizations_controller_spec.rb
+++ spec/requests/admin/organizations_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Admin::OrganizationsController, type: :request do
   let(:superadmin) { create(:user, :superadmin) }
-
+  let(:organization) { create(:organization) }
   before { sign_in superadmin }
-
+  
   describe "organizations" do
     it "returns http success if signed in as superadmin" do
       get '/admin/organizations'
@@ -12,7 +12,7 @@ RSpec.describe Admin::OrganizationsController, type: :request do
     end
 
     it "Must render admin/organizations/show" do
-      get "/admin/organizations/"
+      get admin_organization_path(organization)
       expect(response).to have_http_status(:success)
     end
   end

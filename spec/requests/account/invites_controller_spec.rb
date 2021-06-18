5aecf94b074eb8ba737eb15b5f554197592964ec
dimk0o_ <dima.azarenkoo@gmail.com>
2021-06-18 14:45:15 +0300

    refactored

--- spec/requests/account/invites_controller_spec.rb
+++ spec/requests/account/invites_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Account::InvitesController, type: :request do
 
   describe "GET #new" do
     it "returns http success if signed in as admin" do
-      get account_invites_path
+      get new_account_invite_path
       expect(response).to have_http_status(:success)
     end
   end

92eb4d12014960984a521f300dc21f4d22a3d4b4
Diana <dianaporogy@gmail.com>
2021-06-21 12:10:14 +0300

    Deleting empty files

--- spec/requests/account/favorite_gifts_spec.rb
+++ spec/requests/account/favorite_gifts_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "FavoriteGifts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET #index" do
+    it "returns http success" do
+      get account_favorite_gifts_path
+      expect(response).to have_http_status(:success)
+    end
   end
 end

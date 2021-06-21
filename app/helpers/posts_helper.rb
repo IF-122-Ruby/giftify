c90ad3c55efe02945a9a747615678ae34316de4d
Diana <dianaporogy@gmail.com>
2021-06-21 17:52:32 +0300

    Fix comment

--- app/helpers/posts_helper.rb
+++
@@ -1,9 +0,0 @@
-module PostsHelper
-  def post_menu
-    if current_page?(controller: 'posts', action: 'index')
-      content_tag(:ul, class: 'btn-toggle-nav list-unstyled fw-normal pb-1 small') do
-        render 'posts/categories'
-      end
-    end
-  end
-end

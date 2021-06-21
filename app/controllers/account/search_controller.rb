a6565a9c50566fadb01ad9463f9e223eadd7890d
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-21 15:17:31 +0300

    fix

--- app/controllers/account/search_controller.rb
+++ app/controllers/account/search_controller.rb
@@ -1,7 +1,7 @@
 class Account::SearchController < Account::AccountsController
   def search
     if params[:search].blank?
-      redirect_to(root_url, notice: "Write search request") and return
+      redirect_to(account_gifts_path, notice: "Write search request") and return
     else
       @results = Elasticsearch::Model.search(params[:search].to_s, [User, Gift]).records.to_a
     end

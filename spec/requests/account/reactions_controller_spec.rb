7b035284c652e9c75cb00bc3c1b39ba49c05f315
Cassinipy <mischa.blaster@gmail.com>
2021-06-22 13:22:57 +0300

    add test

---
+++ spec/requests/account/reactions_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Account::Reactions", type: :request do
+  let(:admin) { create(:user, :admin) }
+  let(:micropost) { create(:micropost, author: admin, organization: admin.organization) }
+  let(:reaction_attribute) { attributes_for(:reaction, micropost: micropost, id: micropost.id) }
+
+  before { sign_in admin }
+
+  describe 'reaction' do
+    it 'add and remove reaction to micropost' do
+      post account_reactions_path(reaction_attribute), xhr: true
+      get account_microposts_path
+
+      expect(response.body).to include("#{Reaction::LAUGH} 1")
+
+      delete account_reaction_path(reaction_attribute), xhr: true
+      get account_microposts_path
+
+      expect(response.body).to include("#{Reaction::LAUGH} 0")
+    end
+  end
+end

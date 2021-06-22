6956939c6c673af6b4772c174e2ebf431b91f74b
Cassinipy <mischa.blaster@gmail.com>
2021-06-22 09:04:12 +0300

    add annotate

--- spec/factories/reactions.rb
+++ spec/factories/reactions.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: reactions
+#
+#  id                :bigint           not null, primary key
+#  reaction          :string
+#  reactionable_type :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  reactionable_id   :bigint
+#  user_id           :bigint
+#
+# Indexes
+#
+#  index_reactions_on_reactionable  (reactionable_type,reactionable_id)
+#  index_reactions_on_user_id       (user_id)
+#
 FactoryBot.define do
   factory :reaction do
     user

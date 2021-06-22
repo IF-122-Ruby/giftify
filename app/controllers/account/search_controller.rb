ef58b87fb2c4246b9757748c16761925cba2dbfb
SerhiiPosternak <goextremeforall@gmail.com>
2021-06-22 15:07:35 +0300

    fix bug

--- app/controllers/account/search_controller.rb
+++ app/controllers/account/search_controller.rb
@@ -1,11 +1,11 @@
 class Account::SearchController < Account::AccountsController
   def search
     if params[:search].blank?
-      redirect_to(account_gifts_path, notice: "Write search request") and return
+      redirect_to(account_gifts_path, notice: "Write search request")
     else 
       @results = Elasticsearch::Model.search(params[:search].to_s, [User, Gift]).records.to_a
       if @results.blank?
-        redirect_to(account_gifts_path, notice: "Records not found") and return
+        redirect_to(account_gifts_path, notice: "Records not found")
       end
     end
   end

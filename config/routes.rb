7a27081e2357ca97b7a2f8f383a5d62d6fe7ecb5
VasylenchukMischa <38836502+VasylenchukMischa@users.noreply.github.com>
2021-06-23 10:43:00 +0300

    Add reactions (#68)
    * styling buttons
    
    * add migration
    
    * add spec
    
    * add test
    
    * refactor to slim
    
    * raise variables outside the partial
    
    * add variables

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :microposts, path: :feed, except: [:show, :edit]
     resources :gifts
     resources :users, except: [:create, :new]
-    resource :organization,
+    resource  :organization,
               only: [:edit, :update],
               controller: :organization
     resource  :profile,
@@ -24,19 +24,16 @@ Rails.application.routes.draw do
     resources :organization_gifts, path: :rewards, only: [:index, :show]
     resources :invites, except: %i[edit update]
     resources :notifications, only: :index
-    
+    resources :reactions, only: [:create, :destroy]
     resources :favorite_gifts, only: [:index, :create, :destroy]
-    
     resource :transaction, only: [:create, :new], controller: :transaction
   end
 
   resources :posts
 
   namespace :admin do
-    resources :organizations, only: [:index, :show]
     root to: 'admin#index'
-
+    resources :organizations, only: [:index, :show]
     resources :feedbacks, only: [:index, :show]
-
   end
 end

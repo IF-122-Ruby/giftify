9cb2080cbca52563e8c13bdccf12da5a30fec49b
VasylenchukMischa <38836502+VasylenchukMischa@users.noreply.github.com>
2021-06-29 22:50:17 +0300

    add delete comment functional (#86)
    * add delete comment functional
    
    * update tests
    
    * fix comments
    
    * refactor

--- app/controllers/account/comments_controller.rb
+++ app/controllers/account/comments_controller.rb
@@ -12,13 +12,23 @@ class Account::CommentsController < Account::AccountsController
     end
   end
 
+  def destroy
+    @comment = policy_scope([:account, Comment]).find(params[:id])
+    authorize [:account, @comment]
+    @comment.delete
+    respond_to do |format|
+      format.js
+      format.html { redirect_back fallback_location: root_path, flash: { notice: 'comment deleted' } }
+    end
+  end
+
   private
 
-    def resource
-      current_organization.microposts.find(params[:micropost])
-    end
+  def resource
+    current_organization.microposts.find(params[:micropost])
+  end
 
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end

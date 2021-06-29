25340927bcfe875e46b9b7e5cf92b0a026f1b34c
Diana <dianaporogy@gmail.com>
2021-06-29 09:39:49 +0300

    Edit files

--- spec/requests/accept_invites_spec.rb
+++ spec/requests/accept_invites_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe AcceptInvitesController, type: :request do
   let!(:user) { create(:user) }
   let!(:invite) { create(:invite) }
-  
+
     describe "GET #new" do
       it 'returns success status' do
         get new_accept_invite_path
         expect(response).to have_http_status(:success)
       end
     end
-    
+
     describe "POST #create" do
       it 'changes user count by 1 if given params valid' do
         expect do
@@ -18,6 +18,7 @@ RSpec.describe AcceptInvitesController, type: :request do
             user: {
               token: invite.invite_token,
               email: invite.recipient_email,
+              role: invite.recipient_role,
               first_name: 'Han',
               last_name: 'Solo',
               password: 'Sesam123',

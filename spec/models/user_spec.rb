b1614f84f2432c960d5e63f414670ec04c469cb8
Sonia <glushchak02@gmail.com>
2021-07-01 20:12:05 +0300

    add user token, return balance

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  token                  :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #

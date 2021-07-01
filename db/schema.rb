7d00baa1129ecca71b64856b6a786dbe713f21c0
Sonia <glushchak02@gmail.com>
2021-07-01 22:11:29 +0300

    fix comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_183649) do
+ActiveRecord::Schema.define(version: 2021_07_01_180922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

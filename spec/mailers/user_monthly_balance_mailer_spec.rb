2d698bb200f4ade114799fe592c54b0e627b6e0e
dimk0o_ <dima.azarenkoo@gmail.com>
2021-07-05 16:50:59 +0300

    refactored

--- spec/mailers/user_monthly_balance_mailer_spec.rb
+++ spec/mailers/user_monthly_balance_mailer_spec.rb
@@ -1,11 +1,17 @@
 require "rails_helper"
-require 'sidekiq/testing'
+require "sidekiq/testing"
 
 RSpec.describe UserMonthlyBalanceMailer, type: :mailer do
-  let!(:user)  { create(:user) }
-  let!(:mail)  { UserMonthlyBalanceMailer.balance_email(user) }
 
-  it 'checks deliveries count' do
-    expect(mail.subject).to eq("Your current balance on Giftify") 
+  describe "balance mailer" do
+    let!(:user)  { create(:user, :with_organization) }
+    let!(:transaction) { create(:transaction, receiver: user, sender: user.organization, amount: 100) }
+    let!(:mail)  { UserMonthlyBalanceMailer.balance_email(user) }
+    
+    it "checks deliveries count" do
+      expect(mail.subject).to eq("Your current balance on Giftify")
+      expect(mail.body.encoded).to include("Your current balance on Giftify is: #{user.balance} points! Do not forget to buy something cool ;)")
+    end
   end
 end
+

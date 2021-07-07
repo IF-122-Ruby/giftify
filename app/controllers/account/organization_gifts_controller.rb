067f090e30e7b726d37b4867d46e423d74ab1393
Cassinipy <mischa.blaster@gmail.com>
2021-07-07 11:25:25 +0300

    add buttons and styles

--- app/controllers/account/organization_gifts_controller.rb
+++ app/controllers/account/organization_gifts_controller.rb
@@ -1,10 +1,15 @@
 class Account::OrganizationGiftsController < Account::AccountsController
+  before_action :add_index_breadcrumb, only: :show
+
   def index
-    @gifts = collection.paginate(page: params[:page], per_page: 9)
+    @q = collection.paginate(page: params[:page], per_page: 9).ransack(params[:q])
+    @gifts = @q.result
+    add_breadcrumb('Organization Gifts')
   end
 
   def show
     @gift = resource
+    add_breadcrumb(@gift.id)
   end
 
   private
@@ -15,5 +20,9 @@ class Account::OrganizationGiftsController < Account::AccountsController
 
   def resource
     collection.find(params[:id])
+  end
+
+  def add_index_breadcrumb
+    add_breadcrumb('Organization Gifts', account_organization_gifts_path)
   end
 end

7f121b8efb79f3ec64e1a2c86a2c07ea8ae4fa28
everlastroman <everlast.roman@gmail.com>
2021-07-08 14:37:07 +0300

    fix-2

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -3,10 +3,9 @@ require 'rails_helper'
 RSpec.describe "Articles", type: :request do
   describe "GET #help" do
     let!(:article) { create(:article, :help_page) }
-    
+
     it "correct page" do
       get help_path
-      binding.pry
       expect(response.body).to include("<CENTER> Hello")
     end
   end

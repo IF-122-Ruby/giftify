93df2193aaffeb3a50a3d45d5e168d3c0c67d1d3
everlastroman <everlast.roman@gmail.com>
2021-07-08 10:47:05 +0300

    fix controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,16 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update]
   
   def help
-    @article = Article.find_by(page_name: "help")
+    @article = Article.first
   end
 
   def edit
+    @article = Article.find(params[:id])
     authorize @article
   end
 
   def update
+    @article = Article.find(params[:id])
     authorize @article
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated."
@@ -20,10 +21,6 @@ class ArticlesController < ApplicationController
   end
 
   private
-
-  def set_article
-    @article = Article.find(params[:id])
-  end
 
   def article_params
     params.require(:article).permit(:title, :body)

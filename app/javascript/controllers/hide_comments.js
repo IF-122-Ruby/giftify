16bdca4a73ff987bb0bddc1d20eefb39a1a02ac7
SerhiiPosternak <goextremeforall@gmail.com>
2021-07-09 23:05:06 +0300

    all works

--- app/javascript/controllers/hide_comments.js
+++ app/javascript/controllers/hide_comments.js
@@ -1,24 +1,3 @@
-// function hideComments(elementID) { 
-//   let hidden_comments = document.getElementById(elementID);
-//   if (hidden_comments.style.display === 'none') {
-//       hidden_comments.style.display = 'block';
-//   } else {
-//     hidden_comments.style.display = 'none';
-//   }
-//  }
-
-function hideComments(elementID){
-  let hidden_comments = document.getElementById(elementID);
-  if (hidden_comments.classList.contains('d-none')) {
-      hidden_comments.classList.remove('d-none')
-      hidden_comments.classList.add('d-block')
-  }
-  else {
-    hidden_comments.classList.remove('d-block')
-    hidden_comments.classList.add('d-none')
-  }
-}
-
 function hideComments(elementID){
   let hidden_comments = document.getElementById(elementID);
     hidden_comments.classList.toggle('d-none')

60ffeb126c461c23babb14b827be5c9db1a10329
SerhiiPosternak <goextremeforall@gmail.com>
2021-07-09 23:04:12 +0300

    with wto different js hide comments

--- app/javascript/controllers/hide_comments.js
+++ app/javascript/controllers/hide_comments.js
@@ -1,10 +1,27 @@
-function hideComments(elementID) { 
-  let hidden_comments = document.getElementById(elementID);
-  if (hidden_comments.style.display === 'none') {
-      hidden_comments.style.display = 'block';
-  } else {
-    hidden_comments.style.display = 'none';
-  }
- }
+// function hideComments(elementID) { 
+//   let hidden_comments = document.getElementById(elementID);
+//   if (hidden_comments.style.display === 'none') {
+//       hidden_comments.style.display = 'block';
+//   } else {
+//     hidden_comments.style.display = 'none';
+//   }
+//  }
 
- window.hideComments = hideComments;
+function hideComments(elementID){
+  let hidden_comments = document.getElementById(elementID);
+  if (hidden_comments.classList.contains('d-none')) {
+      hidden_comments.classList.remove('d-none')
+      hidden_comments.classList.add('d-block')
+  }
+  else {
+    hidden_comments.classList.remove('d-block')
+    hidden_comments.classList.add('d-none')
+  }
+}
+
+function hideComments(elementID){
+  let hidden_comments = document.getElementById(elementID);
+    hidden_comments.classList.toggle('d-none')
+}
+
+window.hideComments = hideComments;

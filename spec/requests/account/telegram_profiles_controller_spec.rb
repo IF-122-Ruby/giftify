b3edf5106629285b64bc0e76667379f6808831ba
Cassinipy <mischa.blaster@gmail.com>
2021-07-12 14:52:52 +0300

    small change

--- spec/requests/account/telegram_profiles_controller_spec.rb
+++ spec/requests/account/telegram_profiles_controller_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Account::TransactionController, type: :request do
   before { sign_in user }
 
   describe '/telegram_profile/connect' do
-    it 'create connection' do
+    it 'create connection to telegram' do
       get account_telegram_profile_connect_path(connection_token: telegram_profile.connection_token)
       expect(response).to have_http_status(:redirect)
       follow_redirect!
       expect(response.body).to include('You`re connected to telegram')
     end
 
-    it 'doesn`t create connection' do
+    it 'doesn`t create connection to telegram' do
       get account_telegram_profile_connect_path(connection_token: telegram_profile.connection_token + 'n')
       expect(response).to have_http_status(:redirect)
       follow_redirect!

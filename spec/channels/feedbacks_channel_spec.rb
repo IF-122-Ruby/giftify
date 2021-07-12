3d07f1a21e99635617a1d94f816c65e59f020f4d
dimk0o_ <dima.azarenkoo@gmail.com>
2021-07-12 22:50:30 +0300

    refactored channel test

--- spec/channels/feedbacks_channel_spec.rb
+++ spec/channels/feedbacks_channel_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.describe FeedbacksChannel, type: :channel do
+  let!(:feedback) { create(:feedback) }
+
   it "successfully subscribes" do
-    subscribe
+    subscribe feedback: feedback
     expect(subscription).to be_confirmed
   end
 end

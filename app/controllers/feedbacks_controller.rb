7a7953c692a5758c41add16648589cc6e259a9e4
dimk0o_ <dima.azarenkoo@gmail.com>
2021-07-12 22:32:58 +0300

    refactored due to comments

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -11,7 +11,7 @@ class FeedbacksController < ApplicationController
         format.html { redirect_to root_path, notice: "#{@feedback.name}, thank you for your feedback. The administration will contact you shourtly." }
         ActionCable.server.broadcast 'feedbacks', message: "New feedback created!"
       else
-        render :new
+        format.html { render :new }
       end
     end
   end

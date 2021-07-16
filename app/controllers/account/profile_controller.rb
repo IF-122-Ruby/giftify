583e53bb79c0dc4bca734bafd663dbd58f96a593
VasylenchukMischa <38836502+VasylenchukMischa@users.noreply.github.com>
2021-07-16 08:51:23 +0300

    Fix before demo (#112)
    * asd
    
    * fix mistakes
    
    * fix responsive sidebar

--- app/controllers/account/profile_controller.rb
+++ app/controllers/account/profile_controller.rb
@@ -21,6 +21,6 @@ class Account::ProfileController < Account::AccountsController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :avatar)
   end
 end
